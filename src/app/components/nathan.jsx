26ea6147897bb7ce9fe5f2eb724ac4148f4b9440
The_Ultimate_Sequeira <117172083+Nathan-sudo-pycharm@users.noreply.github.com>
2024-01-18 18:58:08 +0530

    updated by Nathan

---
+++ src/app/components/nathan.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nathan = () => {
+  return (
+    <div>
+      <h1 className="text-red-600  font-bold">This is done by Nathan</h1>
+    </div>
+  );
+};
+
+export default Nathan;
