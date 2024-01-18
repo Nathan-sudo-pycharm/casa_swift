7467d23c1c271d626345ca13a051553b498c9331
Osheen Judith Dcosta <137905130+judy026@users.noreply.github.com>
2024-01-18 20:01:29 +0530

    osheen commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Nathan from "./components/nathan";
+import Osheen from "./components/osheen";
 
 export default function Home() {
   return (
     <div>
       <Nathan />
+      <Osheen />
     </div>
   );
 }
